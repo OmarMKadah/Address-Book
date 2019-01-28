7ae27076befbf609f2b6b17ba60cd73658e3ac1f
Omar Kadah <omarmkadah@gmail.com>
2019-01-27 22:42:46 -0800

    add new contact function to address book

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-function contact (firstName, lastName, phoneNumber) {
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = []
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName,
   this.lastName = lastName,
   this.phoneNumber = phoneNumber
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
 }
